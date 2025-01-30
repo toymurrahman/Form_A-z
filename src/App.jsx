e5686fe6aa17e5d69ff3b98c67e3d73e2d06d45e
toymurrahman <toymurrahman2@gmail.com>
2025-01-30 09:56:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import RefForm from './components/RefForm/RefForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+     {/* <SimpleForm/> */}
+     <RefForm/>
+       
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/RefForm/RefForm.jsx
@@ -0,0 +1,23 @@
+
+
+const RefForm = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+      
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="password" name="password" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default RefForm;
